ed327db721fcb152a240fe7178a019936c22eb1e
starbase <mhorn71@gmail.com>
2014-07-26 17:02:47 +0100

    Finished controllerControl action.

--- actions/controllerControl.py
+++ actions/controllerControl.py
@@ -1,10 +1,8 @@
 __author__ = 'mark'
-import subprocess
+import os
 import utilities.samplerstatus as samplerstatus
 import ConfigParser
-import signal
 import logging
-import sys
 
 
 ##initialise logger
@@ -16,7 +14,6 @@ config = ConfigParser.RawConfigParser()
 
 def control(buffer0):
 
-    status = None
     value = None
 
     config.read("StarinetBeagleLogger.conf")
@@ -27,14 +24,17 @@ def control(buffer0):
         logger.debug("%s %s", "samplerstatus reports sampler not active", str(samplerstatus.status()))
         status = 0
         if buffer0 == 'Reboot':
-            return status, value
-            subprocess.call("reboot")
-        elif buffer == 'Shutdown':
-            return status, value
-            subprocess.call("shutdown", "-h", "now")
+            os.system('reboot')
+            return 0, value
+        elif buffer0 == 'Shutdown':
+            os.system('shutdown -h now')
+            return 0, value
         else:
             logger.critical("%s %s", "controllerControl INVALID_PARAMETER buffer0", buffer0)
+            return 4, value
     elif samplerstatus.status() == 8000:
         logger.debug("%s %s", "samplerstatus reports sampler active", str(samplerstatus.status()))
         status = 8002
         return status, value
+
+
